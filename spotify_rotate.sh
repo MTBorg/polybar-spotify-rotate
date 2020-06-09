00a19ca421024274c8c463622b56fbcc1c69095f
Martin Terneborg <martinterneborg@protonmail.com>
2020-06-09 21:49:41 +0200

    Refactor code

--- spotify_rotate.sh
+++ spotify_rotate.sh
@@ -5,33 +5,32 @@ function rotate_string(){
 		[[ -n $TAIL ]] && echo "${1:$INDEX:$DIST} $TAIL" || echo "${1:$INDEX:$DIST}"
 }
 
-function spot(){
-	DIST=$1
-	INDEX=0
-	while true; do
-		sleep 1 &
-		wait
-		meta_data=$(
-			dbus-send \
-				--print-reply \
-				--dest=org.mpris.MediaPlayer2.spotify \
-				/org/mpris/MediaPlayer2 \
-				org.freedesktop.DBus.Properties.Get \
-				string:org.mpris.MediaPlayer2.Player \
-				string:Metadata)
-		song=$(
-				echo "$meta_data" \
-			| sed -n '/title/{n;p}' \
-			| cut -d '"' -f 2)
-		artist=$(
-				echo "$meta_data" \
-			| sed -n '/artist/{n;n;p}' \
-			| cut -d '"' -f 2)
-		string="$song - $artist -"
-		echo "$(rotate_string "$string" $INDEX $DIST)"
-		INDEX=$((INDEX + 1))
-		[ $INDEX -ge ${#string} ] && INDEX=0
-	done
-}
+DIST=20
+INDEX=0
+while true; do
+	sleep 1 &
+	wait
 
-spot 20
+	meta_data=$(
+		dbus-send \
+			--print-reply \
+			--dest=org.mpris.MediaPlayer2.spotify \
+			/org/mpris/MediaPlayer2 \
+			org.freedesktop.DBus.Properties.Get \
+			string:org.mpris.MediaPlayer2.Player \
+			string:Metadata)
+	song=$(
+			echo "$meta_data" \
+		| sed -n '/title/{n;p}' \
+		| cut -d '"' -f 2)
+	artist=$(
+			echo "$meta_data" \
+		| sed -n '/artist/{n;n;p}' \
+		| cut -d '"' -f 2)
+	string="$song - $artist -"
+
+	echo "$(rotate_string "$string" $INDEX $DIST)"
+
+	INDEX=$((INDEX + 1))
+	[ $INDEX -ge ${#string} ] && INDEX=0
+done
