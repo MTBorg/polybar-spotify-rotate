0f36b46fe19ad4c1eef6f05418b1aaeb6132036b
Martin Terneborg <martinterneborg@protonmail.com>
2020-06-09 21:39:19 +0200

    Fix issue with loop

--- spotify_rotate.sh
+++ spotify_rotate.sh
@@ -1,33 +1,37 @@
 function rotate_string(){
-	DIST=$2
-	INDEX=0
-	while [[ true ]]; do
-		sleep 1
+		INDEX=$2
+		DIST=$3
 		[ $(($DIST+$INDEX-1)) -ge ${#1} ] && TAIL=${1:0:$(($DIST-${#1}+$INDEX))}
-		[[ -n $TAIL ]] && echo "${1:$INDEX:$DIST} $TAIL" || echo ${1:$INDEX:$DIST}
-		INDEX=$((INDEX + 1))
-		[ $INDEX -ge ${#1} ] && INDEX=0
-		unset TAIL
-	done
+		[[ -n $TAIL ]] && echo "${1:$INDEX:$DIST} $TAIL" || echo "${1:$INDEX:$DIST}"
 }
 
 function spot(){
-	meta_data=$(
-		dbus-send \
-			--print-reply \
-			--dest=org.mpris.MediaPlayer2.spotify \
-			/org/mpris/MediaPlayer2 \
-			org.freedesktop.DBus.Properties.Get \
-			string:org.mpris.MediaPlayer2.Player \
-			string:Metadata)
-	song=$(
-			echo $meta_data \
-		| sed -n '/title/{n;p}' \
-		| cut -d '"' -f 2)
-	artist=$(
-			echo $meta_data \
-		| sed -n '/artist/{n;n;p}' \
-		| cut -d '"' -f 2)
-	rotate_string "$song - $artist" $1
+	DIST=$1
+	INDEX=0
+	while true; do
+		sleep 1 &
+		wait
+		meta_data=$(
+			dbus-send \
+				--print-reply \
+				--dest=org.mpris.MediaPlayer2.spotify \
+				/org/mpris/MediaPlayer2 \
+				org.freedesktop.DBus.Properties.Get \
+				string:org.mpris.MediaPlayer2.Player \
+				string:Metadata)
+		song=$(
+				echo "$meta_data" \
+			| sed -n '/title/{n;p}' \
+			| cut -d '"' -f 2)
+		artist=$(
+				echo "$meta_data" \
+			| sed -n '/artist/{n;n;p}' \
+			| cut -d '"' -f 2)
+		string="$song - $artist -"
+		echo "$(rotate_string "$string" $INDEX $DIST)"
+		INDEX=$((INDEX + 1))
+		[ $INDEX -ge ${#string} ] && INDEX=0
+	done
 }
 
+spot 20
